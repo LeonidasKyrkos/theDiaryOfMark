3be0f41df768d82201c7bd46a97d36b5ab834c78
Leo Kyrkos <Leo.Kyrkos@yourfavouritestory.com>
2017-07-27 17:04:46 +0100

    made mark's image an easter egg and added next quote functionality to arrow keys

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="outerwrap">
-		<span class="logo__wrap">
+		<span class="logo__wrap" v-bind:class="{ hide: !logoActive }">
 			<img src="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/39059_10150239415975434_7563380_n.jpg?oh=1534a11fb7ee8d4c400de7478c7324ac&oe=5A0349F6" alt="AEF Mark Horner" class="logo">
 		</span>		
 		<span class="phrase" v-bind:class="{ active: phrase.value.length }">{{phrase.value}}</span>
@@ -19,7 +19,10 @@ export default {
 			phrase: {
 				value: '',
 				desc: ''
-			}
+			},
+			easterEggArr: [],
+			easterEggTarget: 'sherm'.split(''),
+			logoActive: false
 		}
 	},
 	firebase: {
@@ -30,16 +33,39 @@ export default {
 			}
 		}
 	},
+	created() {
+		window.addEventListener('keydown',this.onKeydown.bind(this));
+	},
 	methods: {
 		updatePhrase() {
-			let random = Math.floor(Math.random() * this.phrases.length - 0.1);
-			
+			let random = this.getRandom();
+
 			if(this.phrases[random]) {
 				this.phrase.value = this.phrases[random].value;
 				this.phrase.desc = this.phrases[random].desc || '';
+				this.phrases.splice(random,1);
 			} else {
 				this.phrase.value = this.phrases[0].value;
 				this.phrase.desc = this.phrases[0].desc || '';
+			}
+		},
+		getRandom() {
+			return Math.floor(Math.random() * this.phrases.length - 0.1);
+		},
+		onKeydown(e) {
+			(e.which === 37 || e.which === 39) && this.updatePhrase();
+
+			this.easterEgg(e.key);
+		},
+		easterEgg(key) {
+			if(key === this.easterEggTarget[this.easterEggArr.length]) {
+				this.easterEggArr.push(key);
+			} else {
+				this.easterEggArr = [];
+			}
+
+			if(this.easterEggArr.join('') === this.easterEggTarget.join('')) {
+				this.logoActive = true;
 			}
 		}
 	}
